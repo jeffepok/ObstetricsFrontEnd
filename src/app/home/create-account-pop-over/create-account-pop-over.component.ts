9596dc53f7121e79a13b6a1504be5675acab2051
jefferson <jeffepok@gmail.com>
2021-01-17 19:40:19 +0000

    Initial commit

---
+++ src/app/home/create-account-pop-over/create-account-pop-over.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account-pop-over',
+  templateUrl: './create-account-pop-over.component.html',
+  styleUrls: ['./create-account-pop-over.component.scss'],
+})
+export class CreateAccountPopOverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { Validators, FormBuilder, FormGroup} from '@angular/forms';
+import { PopoverController } from '@ionic/angular';
+import { CreateAccountPopOverComponent } from './create-account-pop-over/create-account-pop-over.component';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    public popoverController: PopoverController
+  ) {}
 
-  constructor() {}
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: CreateAccountPopOverComponent,
+      event: ev,
+      translucent: true,
+      animated: true
+    });
+    return await popover.present();
+  }
 
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name: ['', [Validators.required,Validators.maxLength(30)]],
+      phone: ['', [Validators.required]],
+      email: ['', [Validators.email]]})
+  }
 }
